c740881392786670dbf77b8e1382a4e94c7dd250
Jesus Alexander Martinez Rivera <alexander.code.developer@gmail.com>
2024-05-25 18:08:12 -0600

    Version 1.0.0

---
+++ data.js
@@ -0,0 +1,93 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-landscape-3531355_1920",
+      "name": "landscape-3531355_1920",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 480,
+      "initialViewParameters": {
+        "yaw": -2.5372670159138,
+        "pitch": -0.04357451090252695,
+        "fov": 1.3572162071804879
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.638821686144542,
+          "pitch": -0.1686077337381615,
+          "rotation": 0,
+          "target": "1-bryce-canyon-by-jess-beauchemin"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.435663320271974,
+          "pitch": -0.17921753524414896,
+          "title": "Desert",
+          "text": "Muevete a desierto"
+        }
+      ]
+    },
+    {
+      "id": "1-bryce-canyon-by-jess-beauchemin",
+      "name": "Bryce-Canyon-By-Jess-Beauchemin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09848870926335707,
+          "pitch": 0.034569934621339726,
+          "rotation": 0,
+          "target": "0-landscape-3531355_1920"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.35672451869956134,
+          "pitch": 0.03213861398714535,
+          "title": "Articp",
+          "text": "ir al artico<div><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
